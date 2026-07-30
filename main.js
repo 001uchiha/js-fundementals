e82cef543358a09ed3ae22e22b58c99ffd396452
001uchiha <mohaking321@gmail.com>
2026-07-30 22:11:02 +0300

    lesson 02

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 // Print the greeting on a single line below.
-console.log('Hello, JavaScript!');
+const lines = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
+const a = Number(lines[0]);
+const b = Number(lines[1]);
+console.log(a + b)
