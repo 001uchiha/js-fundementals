73891bef54702348d9007f90079cfff51ac68e33
001uchiha <mohaking321@gmail.com>
2026-07-30 22:33:36 +0300

    lesson 05

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Print the greeting on a single line below.
-const word = require('fs').readFileSync(0, 'utf-8').trim();
-const result = word.split('').reverse().join('');
-console.log(result);
+const lines = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
+const name = lines[0];
+const age = Number(lines[1]);
 
+console.log(`Hi, ${name}! You are ${age} years old.`);
