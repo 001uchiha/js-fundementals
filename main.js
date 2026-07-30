8e46fba7d6b31b0654a90d5017650dc7b84d259f
001uchiha <mohaking321@gmail.com>
2026-07-30 23:09:33 +0300

    lesson 07

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 // Print the greeting on a single line below.
-const n = Number(require('fs').readFileSync(0, 'utf-8').trim().split('\n'));
-if (n % 3 === 0 && n % 5 === 0) {
-  console.log('FizzBuzz');
-} else if (n % 3 === 0) {
-  console.log('Fizz');
-} else if (n % 5 === 0) {
-  console.log('Buzz');
-} else console.log(n);
-
+const n = Number(require('fs').readFileSync(0, 'utf-8').trim());
+let sum = 0;
+// Loop and accumulate, then print sum.
+for (let i = 1; i <= n; i++) sum += i;
+console.log(sum);
