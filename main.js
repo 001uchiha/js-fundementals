2f3f63ce14095fffd6f02f70f189e8ee8b666304
001uchiha <mohaking321@gmail.com>
2026-07-30 22:28:04 +0300

    lesson 04

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 // Print the greeting on a single line below.
-const lines = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
-const a = Number(lines[0]);
-const b = Number(lines[1]);
-const c = Number(lines[2]);
+const word = require('fs').readFileSync(0, 'utf-8').trim();
+const result = word.split('').reverse().join('');
+console.log(result);
 
-const z = Math.floor((a + b + c) / 3);
-console.log(z);
