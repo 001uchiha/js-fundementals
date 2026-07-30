d96e90a2efe6b9fa21f30c0f6909894ccc04212b
001uchiha <mohaking321@gmail.com>
2026-07-30 23:04:02 +0300

    lesson 06

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 // Print the greeting on a single line below.
-const lines = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
-const name = lines[0];
-const age = Number(lines[1]);
+const n = Number(require('fs').readFileSync(0, 'utf-8').trim().split('\n'));
+if (n % 3 === 0 && n % 5 === 0) {
+  console.log('FizzBuzz');
+} else if (n % 3 === 0) {
+  console.log('Fizz');
+} else if (n % 5 === 0) {
+  console.log('Buzz');
+} else console.log(n);
 
-console.log(`Hi, ${name}! You are ${age} years old.`);
