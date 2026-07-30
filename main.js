bf4ef6ec1d78c01a9552b20e7cfd2e773c99c0da
001uchiha <mohaking321@gmail.com>
2026-07-30 22:20:46 +0300

    lesson 03

--- main.js
+++ main.js
@@ -2,4 +2,7 @@
 const lines = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
 const a = Number(lines[0]);
 const b = Number(lines[1]);
-console.log(a + b)
+const c = Number(lines[2]);
+
+const z = Math.floor((a + b + c) / 3);
+console.log(z);
